f8f9e68f895e021616a9ce17460d16efcf1e1b5d
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-07-25 13:13:06 -0700

    Fix: Don't do symetric diff

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ function arrayDiff(a1, a2) {
   for (i=0, len=a1.length; i<len; i++) { o1[a1[i]] = true; }
   for (i=0, len=a2.length; i<len; i++) { o2[a2[i]] = true; }
   for (k in o1) { if (!(k in o2)) { diff.push(k); } }
-  for (k in o2) { if (!(k in o1)) { diff.push(k); } }
+  //for (k in o2) { if (!(k in o1)) { diff.push(k); } }
   return diff;
 }
 
